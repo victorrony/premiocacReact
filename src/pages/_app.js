3e9cee115892c02e1380156ad638a24b4f3dd4be
rodydacruz <rodylag@outlook.com>
2023-03-21 11:21:58 -0100

    erase

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
+import Alerts from '@/components/notification/Alerts'
 import { client } from '@/services/api'
 import '@/styles/globals.css'
 import { ApolloProvider } from '@apollo/client'
 
 export default function App({ Component, pageProps }) {
-  return
-  <ApolloProvider client={client} >
-    <Alerts />
+  return <ApolloProvider client={client} >
+    <Alertsts />
     <Component {...pageProps} />
   </ApolloProvider>
 }
