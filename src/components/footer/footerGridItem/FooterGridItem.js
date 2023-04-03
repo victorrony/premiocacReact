79f0db0535b86cfd1839cdad939f7955c92c60e9
victor Rony <victorrony89@gmail.com>
2023-04-03 17:27:03 -0100

    update footer

--- src/components/footer/footerGridItem/FooterGridItem.js
+++ src/components/footer/footerGridItem/FooterGridItem.js
@@ -2,7 +2,7 @@ export default function FooterGridItem({ img, name }) {
   return (
     <img
       src={process.env.NEXT_PUBLIC_API_URL + img.url}
-      className=" w-24 h-24 lg:w-28 lg:h-28  rounded-[50px]"
+      className=" w-20 h-20 lg:w-28 lg:h-28  rounded-[50px]"
     />
   );
 }
