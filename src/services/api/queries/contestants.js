a6910065b3fd2974a9a6f213aee16c72bff04cd5
victor Rony <victorrony89@gmail.com>
2023-04-05 16:49:25 -0100

    update

--- src/services/api/queries/contestants.js
+++ src/services/api/queries/contestants.js
@@ -2,10 +2,11 @@ const { gql } = require("@apollo/client");
 
 export const CONTESTANTS = gql `
 query Attributes {
-    contestants(pagination:{pageSize:20}) {
-      data {
-        attributes {
-          createdAt
+  contestants(pagination:{pageSize:20} sort:"totalVotes:desc") {
+    data {
+      attributes {
+        totalVotes
+        createdAt
           description
           name
           portrait {
